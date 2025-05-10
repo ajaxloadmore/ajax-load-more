61d18ccd94938823b26a1876cf1de32dfea31b42
Darren Cooney <darren.cooney@gmail.com>
2025-05-10 11:13:57 -0400

    Various security fixes

--- build/frontend/ajax-load-more.asset.php
+++ build/frontend/ajax-load-more.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '9909c29c175a064d9232');
+<?php return array('dependencies' => array(), 'version' => '1485c6fa0240ba1e0604');
--- build/frontend/ajax-load-more.min.asset.php
+++ build/frontend/ajax-load-more.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '552f6033e2f08c4a1be8');
+<?php return array('dependencies' => array(), 'version' => '1485c6fa0240ba1e0604');
